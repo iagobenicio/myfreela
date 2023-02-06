3be8e26fcc375c737dbbabc91f3df296f8f2fa12
Iago <iagobensen@gmail.com>
2023-02-06 20:52:48 -0300

    view e controller de registrar users

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using myfreela.context;
+using myfreela.viewmodels;
 
 namespace myfreela.Controllers
 {   
@@ -13,11 +15,13 @@ namespace myfreela.Controllers
     [Route("[controller]")]
     public class UserController : Controller
     {
-        
-        private readonly MyFreelaContext _context;
-        public UserController(MyFreelaContext context)
+        private readonly UserManager<IdentityUser<int>> _user;
+        private readonly SignInManager<IdentityUser<int>> _sigInManager;
+
+        public UserController(UserManager<IdentityUser<int>> user, SignInManager<IdentityUser<int>> signInManager)
         {
-            _context = context;
+            _user = user;
+            _sigInManager = signInManager;
         }
         
         [HttpGet("login")]
@@ -26,5 +30,34 @@ namespace myfreela.Controllers
             return View();
         }
 
+        [HttpGet("register")]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return NotFound();
+            }
+        
+            return View();
+
+        }
+
+        private IdentityUser<int> MaperViewModelToDbModel(RegisterViewModel userViewModel, IdentityUser<int> userDB)
+        {
+            userDB.Email = userViewModel.Email;
+            userDB.EmailConfirmed = false;
+            userDB.PhoneNumberConfirmed = false;
+            userDB.TwoFactorEnabled = false;
+            userDB.LockoutEnabled = false;
+            userDB.AccessFailedCount = 0;
+
+            return userDB;
+        }
     }
 }
---
+++ viewmodels/RegisterViewModel.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace myfreela.viewmodels
+{
+    public class RegisterViewModel : LoginViewModel
+    {
+        public string? Confpassword { get; set; }
+    }
+}
